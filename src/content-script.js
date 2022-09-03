6cc593582896057102a4875123407c6df26e7831
Roman Kamenivskyi <rk@wwwest.solutions>
2022-09-03 14:38:05 +0300

    add an ability to add copy button to first block of a code

--- src/content-script.js
+++ src/content-script.js
@@ -1,1 +1,26 @@
-console.log("Hello from extension!");
+const codeContainer = document.querySelector(".s-code-block");
+const code = codeContainer.querySelector("code");
+console.log("codeContainer: ", codeContainer);
+console.log("code: ", code);
+
+const copyToClipboard = (textToCopy) => {
+  navigator.clipboard.writeText(textToCopy).then(() => {
+    console.log("successfully copied!");
+  });
+};
+
+const createCopyButton = () => {
+  const button = document.createElement("button");
+  button.style.position = "absolute";
+  button.style.right = "10px";
+  button.style.top = "10px";
+  button.textContent = "Copy";
+
+  button.addEventListener("click", () => copyToClipboard(code.textContent));
+
+  return button;
+};
+
+const copyButton = createCopyButton();
+codeContainer.style.position = "relative";
+codeContainer.appendChild(copyButton);
